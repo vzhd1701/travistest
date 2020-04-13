03dbe111b6a7be288b4c6e6665910c993397c9e4
vzhd1701 <vzhd1701@gmail.com>
2020-04-13 19:26:03 +0500

    Update test_test.py

--- test/test_test.py
+++ test/test_test.py
@@ -3,7 +3,7 @@ import requests
 
 class TestBoop(unittest.TestCase):
     def test_root(self):
-        r = requests.get("https://ipinfo.io/ip")
+        r = requests.get("https://icanhazip.com/")
         print(r.content)
 
 if __name__ == "__main__":
